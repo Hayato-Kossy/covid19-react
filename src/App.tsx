5e585b81d727b5430f14cc0b65df0106e8afede5
Hayato Koshi <kshyt2501@icloud.com>
2022-11-20 18:05:36 +0900

    完成

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import DashBoard from './features/covid/DashBoard/DashBoard';
+
+function App() {
+  return <DashBoard/>
+}
+
+export default App;
